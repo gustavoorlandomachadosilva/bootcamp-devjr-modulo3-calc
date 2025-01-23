01537ca43ebc6fe2b85050dca02f39978db7564d
Gustavo Orlando <gustavoorlando.1d2015@gmail.com>
2025-01-23 13:26:05 -0300

    closes #4 - Operacoes Matematicas

--- javascript.js
+++ javascript.js
@@ -1,6 +1,21 @@
-function calc() {
+function calc(operacao) {
    var num1 = parseFloat(document.getElementById("num1").value);
    var num2 = parseFloat(document.getElementById("num2").value);
-   var resultado = (num1 + num2);
+   var resultado = 0;
+
+   switch (operacao) {
+      case '+': resultado = (num1 + num2);         
+         break;
+      case '-': resultado = (num1 - num2);
+         break;
+      case '*': resultado = (num1 * num2);
+         break;
+      case '/': resultado = (num1 / num2);
+         break;
+      default:
+         break;
+   }
+  
+
    document.getElementById("output").value = resultado;
 }
